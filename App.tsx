2fd0b56fbc06063fa0d4bda643d30c7da0517f3e
Atharva Mishra <atharvamishra956@gmail.com>
2021-01-30 16:21:37 +0530

    added a header and a input box

--- App.tsx
+++ App.tsx
@@ -1,21 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Header } from 'react-native-elements';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <SafeAreaProvider>
+        <View>
+          <Header backgroundColor='#008103' centerComponent={{
+            text: 'Pocket Dictionary',
+            style: { color: '#fff', fontSize: 20 },
+          }} />
+
+          <TextInput
+            style={styles.inputBox}
+            placeholder="Word Here"
+          />
+        </View>
+      </SafeAreaProvider>
+
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  inputBox: {
+    marginTop: 100,
+    width: '80%',
+    alignSelf: 'center',
+    height: 40,
+    textAlign: 'center',
+    borderWidth: 4,
+    outline: 'none',
   },
 });
